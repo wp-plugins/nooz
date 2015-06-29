a5184576d366d9e4678e26e156b92e21ff406b21
mightydigital <mightydigital@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-06-29 17:38:41 +0000

    release 0.5.3
    git-svn-id: https://plugins.svn.wordpress.org/nooz/tags/0.5.3@1189756 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- inc/autoload.php
+++ inc/autoload.php
@@ -1,12 +1,16 @@
 <?php
 
 if ( ! class_exists( '\Symfony\Component\ClassLoader\Psr4ClassLoader' ) ) {
-	require_once( __DIR__ . '/vendor/Symfony/Component/ClassLoader/Psr4ClassLoader.php' );
+    require_once( __DIR__ . '/vendor/Symfony/Component/ClassLoader/Psr4ClassLoader.php' );
 }
 $loader = new \Symfony\Component\ClassLoader\Psr4ClassLoader();
 $loader->addPrefix( 'WPAlchemy\\', __DIR__ . '/vendor/WPAlchemy' );
 $loader->addPrefix( 'WPAlchemy\\Settings', __DIR__ . '/vendor/WPAlchemy' );
 $loader->addPrefix( 'MightyDev\\WordPress\\Plugin', __DIR__ );
 $loader->register();
-require_once(__DIR__ . '/vendor/EDD/EDD_SL_Plugin_Updater.php');
+require_once( __DIR__ . '/vendor/MightyDev/autoload.php' );
+if ( ! class_exists( '\Twig_Autoloader' ) ) {
+    require_once( __DIR__ . '/vendor/Twig/Autoloader.php' );
+    Twig_Autoloader::register();
+}
 require_once(__DIR__ . '/vendor/WPAlchemy/helpers.php');
